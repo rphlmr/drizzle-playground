3c24db57c02d639e44a7301c752de4a820cbf1f8
rphlmr <raphael@uaerom.com>
2023-08-24 17:56:01 +0200

    add comment

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import { db } from "./pg/postgres-js";
 import { cities } from "./pg/schema";
 
 async function main() {
+	// Write your code here
 	console.log(db.select().from(cities).toSQL());
 }
 
