4668498af5c6e3b8e8c2ad524d5a8e8da60fa3af
rphlmr <raphael@uaerom.com>
2024-04-10 10:07:32 +0200

    note about PGLite in memory

--- src/pg/pglite/index.ts
+++ src/pg/pglite/index.ts
@@ -5,3 +5,7 @@ import { drizzle } from "drizzle-orm/pglite";
 export const client = new PGlite("./src/pg/pglite/data.db");
 
 export const db = drizzle(client, { logger: true });
+
+// If you want to use PGLite in memory (without any file persistence)
+// Run your migrations here or in a separate script, but make sure to run them before using the db.
+// await migrate(db, { migrationsFolder: `./src/pg/drizzle` });
